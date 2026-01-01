00c34ba91ac69afa441e5e22b087247a4800e868
Jeremy Alexander Ramirez Galeotti <66746920+Jeremy-Ramirez@users.noreply.github.com>
2026-01-01 23:02:14 +0100

    Make footer year dynamic
    Updated footer to display the current year dynamically.

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  
   return (
     <footer className=" text-white py-8 px-4 sm:px-6 lg:px-8  ">
       <div className="max-w-7xl mx-auto text-center">
         <p className="text-sm text-gray-400">
-          © 2025 Jeremy Ramirez Galeotti. Built with Next.js and Tailwind CSS.
+          © {currentYear} Jeremy Ramirez Galeotti. Computer Science Engineer
         </p>
       </div>
     </footer>
